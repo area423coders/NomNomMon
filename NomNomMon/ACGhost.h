8a8f04c3bd3eacd35490dfe6bc3994fbc3274a58
tgay <tgay@Roddy-Mac-Lab-09.local>
2013-05-07 13:28:44 -0400

    added methods

--- NomNomMon/ACGhost.h
+++ NomNomMon/ACGhost.h
@@ -12,4 +12,55 @@
 
 @interface ACGhost : ACActor
 
+- (void) ghostInit
+{
+    
+}
+
+- (void) panic
+{
+    self.setSpeed (self.getSpeed / 1.5)
+}
+
+- (void) kill
+{
+    // animation for death, send ghost to house, reset to default sprite
+}
+- (BOOL) isAlive
+{
+    if ()
+    {
+        return
+    }
+    else
+    {
+        return false
+    }
+    //is ghost dead? return false, else return true
+}
+
+- (void) respawn
+{
+    //another iteration of the respective ghost into the house
+}
+
+- (void) ghostPathfinding
+{
+    // Search for the mouth and butt of Pac-Man
+}
+
+- (void) getPos
+{
+    // get the position of the ghost
+}
+
+- (void) setSpeed
+{
+    //set speed of the ghost based on current ghost mood
+}
+- (void) getSpeed
+{
+    
+}
+
 @end
