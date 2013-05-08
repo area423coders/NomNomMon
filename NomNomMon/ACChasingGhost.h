7cb890aea7147fea7f75ee9eda2b43481ed1730b
tgay <tgay@Roddy-Mac-Lab-09.local>
2013-05-08 12:48:18 -0400

    added some codes

---
+++ NomNomMon/ACChasingGhost.h
@@ -0,0 +1,13 @@
+//
+//  ACChasingGhost.h
+//  NomNomMon
+//
+//  Created by tgay on 5/8/13.
+//  Copyright (c) 2013 Baylor School. All rights reserved.
+//
+
+#import "ACGhost.h"
+
+@interface ACChasingGhost : ACGhost
+
+@end
--- NomNomMon/ACGhost.h
+++ NomNomMon/ACGhost.h
@@ -10,57 +10,18 @@
 #import <UIKit/UIKit.h>
 #import "ACActor.h"
 
+typedef NS_ENUM(NSInteger, ACGhostState) {
+    ACGhostChasing,
+    ACGhostPanicking,
+    ACGhostWaiting
+};
+
 @interface ACGhost : ACActor
 
-- (void) ghostInit
-{
-    
-}
+@property CGFloat speed;
+@property (nonatomic) ACGhostState state;
 
-- (void) panic
-{
-    self.setSpeed (self.getSpeed / 1.5)
-}
-
-- (void) kill
-{
-    // animation for death, send ghost to house, reset to default sprite
-}
-- (BOOL) isAlive
-{
-    if ()
-    {
-        return
-    }
-    else
-    {
-        return false
-    }
-    //is ghost dead? return false, else return true
-}
-
-- (void) respawn
-{
-    //another iteration of the respective ghost into the house
-}
-
-- (void) ghostPathfinding
-{
-    // Search for the mouth and butt of Pac-Man
-}
-
-- (void) getPos
-{
-    // get the position of the ghost
-}
-
-- (void) setSpeed
-{
-    //set speed of the ghost based on current ghost mood
-}
-- (void) getSpeed
-{
-    
-}
+- (void) didGetEaten;
+- (void) updateDestinationWithLocation: (CGPoint) nomNomMonLocation;
 
 @end
